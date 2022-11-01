857169106e9977f3dd639463674d72c1147bad58
Jakub Turkiewicz <jj.turkiewicz@gmail.com>
2022-11-01 22:22:40 +0100

    ref: moved testbase to restful booker class & bumped deps

--- src/test/java/restfulbooker/e2e/RestfulBookerE2ETests.java
+++ src/test/java/restfulbooker/e2e/RestfulBookerE2ETests.java
@@ -1,10 +1,17 @@
 package restfulbooker.e2e;
 
 import io.restassured.RestAssured;
+import org.assertj.core.api.BDDSoftAssertions;
+import org.assertj.core.api.junit.jupiter.InjectSoftAssertions;
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import restfulbooker.TestBase;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import restfulbooker.core.Tokenizer;
 import restfulbooker.entities.BookingDatesReq;
 import restfulbooker.entities.BookingReq;
 import restfulbooker.entities.BookingResp;
@@ -15,20 +22,44 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Map;
 
 import static common.Constants.AVG_TIMEOUT;
 import static io.restassured.http.ContentType.JSON;
 import static org.apache.http.HttpStatus.SC_CREATED;
 import static org.apache.http.HttpStatus.SC_OK;
+import static org.assertj.core.api.Assumptions.assumeThat;
+import static restfulbooker.core.EnvHolder.RESTFUL_BOOKER_PASSWORD;
+import static restfulbooker.core.EnvHolder.RESTFUL_BOOKER_USERNAME;
 
-class RestfulBookerE2ETests extends TestBase {
-    private SecureRandom random;
+@ExtendWith(SoftAssertionsExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class RestfulBookerE2ETests {
+    private final SecureRandom random = new SecureRandom();
+    @InjectSoftAssertions
+    private BDDSoftAssertions softly;
+    private Tokenizer tokenizer;
     private BookingResp booking;
 
     @BeforeAll
     void preconditions() {
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+        var healthCheck = RestAssured.given().get("/ping");
+        assumeThat(healthCheck.statusCode())
+            .as("`%s` is OFFLINE. Skipping tests...", RestAssured.baseURI)
+            .isEqualTo(SC_CREATED);
+        tokenizer = Tokenizer.INSTANCE;
+        var body = Map.of(
+            "username", RESTFUL_BOOKER_USERNAME,
+            "password", RESTFUL_BOOKER_PASSWORD
+        );
+        var authResponse = RestAssured.given().contentType(JSON).body(body).post("/auth");
+        assumeThat(authResponse.statusCode())
+            .as("Unable to create token. Skipping tests...")
+            .isEqualTo(SC_OK);
+        tokenizer.setToken(authResponse.jsonPath().getString("token"));
         RestAssured.basePath = "/booking";
-        random = new SecureRandom();
     }
 
     @Test
